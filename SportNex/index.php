f5aed8c6d1412b4358a2a08e32cf0497283f1fdd
Mauro Leonel <107223847+MauroLeonel182@users.noreply.github.com>
2024-08-20 17:30:02 -0300

    Add files via upload

--- SportNex/index.php
+++ SportNex/index.php
@@ -1,146 +1,54 @@
-<!DOCTYPE html>
-<html lang="es">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>SportNex</title>
-    <!-- Estilos -->
-    <link rel="icon" href="img/logo.png" type="logo/png">
-    <link rel="stylesheet" href="css/styles.css">
-    <script src="https://kit.fontawesome.com/ed3188a352.js" crossorigin="anonymous"></script>
-</head>
-<body>
-    <header>
-        <div class="container">
-            <div class="logo">
-                <img src="img/logo.png" alt="logo">
-                <h1>SportNex</h1>
-            </div>
-            <nav>
-                <ul>
-                    <li><a href="#">Home</a></li>
-                    <li><a href="#">Horarios</a></li>
-                    <li><a href="#">Login</a></li>
-                    <li><a href="#">Register</a></li>
-                </ul>
-            </nav>
-        </div>
-    </header>
+<?php
+require "config.php";
+$page ="index"; // Página predeterminada
+if(isset($_GET['page'])) {
+    $page = $_GET['page'];
+}
 
-    <main>
-        <section class="hero">
-            <img src="img/hero-image.jpg" alt="Hero Image"> 
-        </section>
+$fecha = isset($_GET['fecha']) ? $_GET['fecha'] : date('Y-m-d');
 
-        <section class="instalaciones">
-            <h2>INSTALACIONES</h2>
-            <div class="grid-container">
-                <div class="grid-item">
-                    <img src="img/cancha1.jpeg" alt="Cancha 1">
-                    <h3>Cancha de Padel N°1</h3>
-                    <p>Cancha de césped sintético.</p>
-                    
-                </div>
-                <div class="grid-item">
-                    <img src="img/cancha2.jpg" alt="Cancha 2">
-                    <h3>Cancha de Padel N°2</h3>
-                    <p>Cancha de césped sintético.</p>
-                    
-                </div>
-                <div class="grid-item">
-                    <img src="img/cancha3.jpg" alt="Cancha 3">
-                    <h3>Cancha de Padel N°3</h3>
-                    <p>Description of second product</p>
-                    
-                </div>
-                <div class="grid-item">
-                    <img src="img/cancha4.jpeg" alt="Cancha 1 Futbol">
-                    <h3>Cancha de Futbol N°1</h3>
-                    <p>Description of third product</p>
-                    
-                </div>
-                <div class="grid-item">
-                    <img src="img/cancha5.jpg" alt="Cancha 2 Futbol">
-                    <h3>Cancha de Futbol N°2</h3>
-                    <p>Description of fourth product</p>
-                    
-                </div>
-                <div class="grid-item">
-                    <img src="img/cancha6.jpg" alt="Cancha 3 Futbol">
-                    <h3>Cancha de Futbol N°3</h3>
-                    <p>Description of fifth product</p>
-                
-                </div>
-            </div>
-        </section>
+switch ($page) {
+    case 'login':
+        require "controlador/LoginController.php";
+        LoginController::index();
+        break;
 
-        <section class="productos">
-            <h2>Heading</h2>
-            <div class="product-grid-container">
-                <div class="product-item large">
-                    <img src="img/product1.jpg" alt="Featured product">
-                    <h3>Featured product</h3>
-                    <p>Description of featured product</p>
-                    
-                </div>
-                <div class="product-item small">
-                    <img src="img/product2.jpg" alt="Product">
-                    <h3>Product</h3>
-                    <p>Description of top product</p>
-                    
-                </div>
-                <div class="product-item small">
-                    <img src="img/product3.jpg" alt="Product">
-                    <h3>Product</h3>
-                    <p>Description of lower product</p>
-                    
-                </div>
-            </div>
-        </section>
+    case 'loginout':
+        require "controlador/LoginController.php";
+        LoginController::login();
+        break;
 
+    case 'logout': 
+        require "controlador/LoginController.php";
+        LoginController::logout();
+        break;
 
+    case 'admin':
+        require "controlador/AdminController.php";
+        AdminController::index();
+        break;
 
-    </main>
+    case 'horarios':
+        require "controlador/ReservaController.php";
+        ReservaController::mostrarHorarios($fecha);
+        break;
 
-    <footer>
-        <div class="container">
-            <div class="logo">
-                <img src="img/logo2.png" alt="logo/png">
-                <h1>SportNex</h1>
-            </div>
-            <div class="footer-links">
-                <div class="footer-column">
-                    <h3>Topic</h3>
-                    <ul>
-                        <li><a href="#">Page</a></li>
-                        <li><a href="#">Page</a></li>
-                        <li><a href="#">Page</a></li>
-                    </ul>
-                </div>
-                <div class="footer-column">
-                    <h3>Topic</h3>
-                    <ul>
-                        <li><a href="#">Page</a></li>
-                        <li><a href="#">Page</a></li>
-                        <li><a href="#">Page</a></li>
-                    </ul>
-                </div>
-                <div class="footer-column">
-                    <h3>Topic</h3>
-                    <ul>
-                        <li><a href="#">Page</a></li>
-                        <li><a href="#">Page</a></li>
-                        <li><a href="#">Page</a></li>
-                    </ul>
-                </div>
-            </div>
-            <div class="social-media">
-                <a href=""><i class="fa-brands fa-facebook"></i></a>
-                <a href=""><i class="fa-brands fa-whatsapp"></i></a>
-                <a href=""><i class="fa-brands fa-youtube"></i></a>
-                <a href=""><i class="fa-brands fa-instagram"></i></a>
-            </div>
-        </div>
-    </footer>
-</body>
-</html>
+    case 'cancha':
+            require "controlador/CanchasController.php";
+            $controller = new CanchasController();
+            if (isset($_GET['opcion'])) {
+                $metodo = $_GET['opcion'];
+                if (method_exists($controller, $metodo)) {
+                    $controller->{$metodo}();
+                }
+                } else {
+                    $controller->listar();
+                }
+    break;
+
+    default:
+        require "controlador/FrontController.php";
+        FrontController::index();
+        break;
+}
+
