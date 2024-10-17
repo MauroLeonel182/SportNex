1b9e7aaab0890b764acf0f8856d52549d2130e9f
Mauro Leonel <107223847+MauroLeonel182@users.noreply.github.com>
2024-10-17 17:27:45 -0300

    Add files via upload

---
+++ config.php
@@ -0,0 +1,6 @@
+<?php
+define("urlsite","http://localhost/SportNex/");
+define("DB_NAME","sportnex");
+define("DB_USER","root");
+define("DB_PASS","");
+date_default_timezone_set('America/Argentina/Buenos_Aires'); // Cambia a la zona horaria que necesites
